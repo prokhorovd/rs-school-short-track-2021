cb2fef4346b942283876eb31a8c09bd1ed0ce188
prokhorovd <github@prokhorov.dev>
2021-04-08 12:21:23 +0400

    feat: task 1 completed

--- src/01-matrix-elements-sum.js
+++ src/01-matrix-elements-sum.js
@@ -14,8 +14,21 @@
  *
  * The result should be 9
  */
-function getMatrixElementsSum(/* matrix */) {
-  throw new Error('Not implemented');
+function getMatrixElementsSum(matrix) {
+  // throw new Error('Not implemented');
+  let result = 0;
+  const excludedRowsList = [];
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i][j] === 0) {
+        excludedRowsList.push(j);
+      }
+      if (excludedRowsList.includes(j) === false) {
+        result += matrix[i][j];
+      }
+    }
+  }
+  return result;
 }
 
 module.exports = getMatrixElementsSum;
