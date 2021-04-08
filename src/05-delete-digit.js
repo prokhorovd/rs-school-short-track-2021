d9d8b5759cc60178b385927c86e9e8b01cb2ae5a
prokhorovd <github@prokhorov.dev>
2021-04-08 18:00:45 +0400

    task 5 completed

--- src/05-delete-digit.js
+++ src/05-delete-digit.js
@@ -9,8 +9,20 @@
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new Error('Not implemented');
+function deleteDigit(input) {
+  let result = 0;
+  // Disassemble number
+  const inputArray = String(input).split('');
+  // Delete every digit one at a time and check if number can become new maximum
+  for (let i = 0; i < inputArray.length; i++) {
+    const modifiedInputArray = inputArray.slice();
+    modifiedInputArray.splice(i, 1);
+    const slicedNum = Number(modifiedInputArray.join(''));
+    if (slicedNum > result) {
+      result = slicedNum;
+    }
+  }
+  return result;
 }
 
 module.exports = deleteDigit;
