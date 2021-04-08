281490bfc09bc1a2de629a38ac81f420c48628ad
prokhorovd <github@prokhorov.dev>
2021-04-08 14:31:39 +0400

    task 3 completed

--- src/03-sort-by-height.js
+++ src/03-sort-by-height.js
@@ -9,8 +9,29 @@
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new Error('Not implemented');
+
+// this func is used for correction of standard sort function
+function sortAssist (a, b) {
+  return a - b;
+}
+
+function sortByHeight(arr) {
+  // Let's found every appearance of elementToKeep. Return sorted array if not found.
+  const elementToKeep = -1;
+  const positionsOfFixedElement = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === elementToKeep) positionsOfFixedElement.push(i);
+  }
+  if (positionsOfFixedElement.length === 0) return arr.sort(sortAssist);
+  // Remove elementToKeep from array, sort array, add elementToKeep to initial positions.
+  while (arr.indexOf(elementToKeep) !== -1) {
+    arr.splice(arr.indexOf(elementToKeep), 1);
+  }
+  const result = arr.sort(sortAssist);
+  for (let i = 0; i < positionsOfFixedElement.length; i++) {
+    result.splice(positionsOfFixedElement[i], 0, -1);
+  }
+  return result;
 }
 
 module.exports = sortByHeight;
