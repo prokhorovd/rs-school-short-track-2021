e3169d3a962670e2cf5f6f25a45dadee07ed8a58
prokhorovd <github@prokhorov.dev>
2021-04-09 16:06:30 +0400

    task 11 completed

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,34 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  if (str.length === 0) return str;
+  let result = '';
+  const stringAsArray = str.split('');
+  let currentChar = stringAsArray[0];
+  let charCoeff = 1;
+  // iterate over chars, compare every one to previous.
+  for (let i = 1; i < stringAsArray.length; i++) {
+    if (stringAsArray[i] === currentChar) {
+      charCoeff += 1;
+    } else {
+      result += String(charCoeff + currentChar);
+      currentChar = stringAsArray[i];
+      charCoeff = 1;
+    }
+    if (i === stringAsArray.length - 1) {
+      result += String(charCoeff + currentChar);
+    }
+  }
+  // remove ones from result
+  let output = '';
+  for (let i = 0; i < result.length; i++) {
+    if (result[i] !== '1') output += result[i];
+  }
+  return output;
 }
+//
+// let str = 'aabbbc';
+// console.log(encodeLine(str))
 
 module.exports = encodeLine;
