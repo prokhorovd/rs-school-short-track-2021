0c7cbfc81987e7128c5d083fe331e2e342aa7701
prokhorovd <github@prokhorov.dev>
2021-04-09 14:18:05 +0400

    task 10 completed

--- src/10-find-index.js
+++ src/10-find-index.js
@@ -11,8 +11,24 @@
  * For ([1, 2, 3], 2) should return 1
  *
  */
-function findIndex(/* array, value */) {
-  throw new Error('Not implemented');
+function findIndex(array, value) {
+  // default values
+  let startIndex = 0;
+  let endIndex = array.length - 1;
+  let middleIndex = Math.ceil(array.length / 2);
+  // while not found split search area by 2
+  while (array[middleIndex] !== value) {
+    if (middleIndex === 1) return 0;
+    if (value > array[middleIndex]) {
+      startIndex = middleIndex;
+      middleIndex = Math.ceil((startIndex + endIndex) / 2);
+    }
+    if (value < array[middleIndex]) {
+      endIndex = middleIndex;
+      middleIndex = Math.ceil((startIndex + endIndex) / 2);
+    }
+  }
+  return middleIndex;
 }
 
 module.exports = findIndex;
