e5b3985263930e1e91c2344dcbb9e5081f0ed03d
prokhorovd <github@prokhorov.dev>
2021-04-09 17:15:34 +0400

    task 12 completed

--- src/12-dns-stats.js
+++ src/12-dns-stats.js
@@ -20,8 +20,27 @@
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new Error('Not implemented');
+function getDNSStats(domains) {
+  const domainPartsList = [];
+  const result = {};
+  // create array with arrays, that contain domain parts
+  for (let i = 0; i < domains.length; i++) {
+    domainPartsList.push(domains[i].split('.').reverse());
+  }
+  // iterate through domain parts array, create names and push them to result object
+  for (let i = 0; i < domainPartsList.length; i++) {
+    let domainName = '.';
+    for (let j = 0; j < domainPartsList[i].length; j++) {
+      // todo
+      if (j === 0) domainName = `.${domainPartsList[i][j]}`;
+      else {
+        domainName = `.${domainPartsList[i].slice(0, j + 1).join('.')}`;
+      }
+      if (domainName in result) result[domainName] += 1;
+      else result[domainName] = 1;
+    }
+  }
+  return result;
 }
 
 module.exports = getDNSStats;
