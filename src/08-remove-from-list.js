d0c3993281367216eadf519d790316b892583267
prokhorovd <github@prokhorov.dev>
2021-04-12 13:45:44 +0400

    task 8 completed

--- src/08-remove-from-list.js
+++ src/08-remove-from-list.js
@@ -7,7 +7,7 @@
  * @return {List}
  *
  * @example
- * For l = [3, 1, 2, 3, 4, 5] and l = 3,
+ * For l = [3, 1, 2, 3, 4, 5] and k = 3,
  * the output should be [1, 2, 4, 5]
  *
  * Singly - linked lists are already defined with this interface
@@ -17,8 +17,30 @@
  * }
  */
 
-function removeKFromList(/* l, k */) {
-  throw new Error('Not implemented');
+function removeKFromList(l, k) {
+  // implement variables to track elements
+  let headOfTheList = l;
+  let currentElement = l;
+  let previousElement = null;
+  // iterate through list
+  while (currentElement) {
+    // match found case
+    if (currentElement.value === k) {
+      if (!previousElement) {
+        currentElement = currentElement.next;
+        headOfTheList = currentElement;
+      } else {
+        previousElement.next = currentElement.next;
+        currentElement = currentElement.next;
+      }
+    }
+    // next step
+    previousElement = currentElement;
+    currentElement = currentElement.next;
+    // if next element doesn't exist - return head
+    if (!currentElement.next) return headOfTheList;
+  }
+  return headOfTheList;
 }
 
 module.exports = removeKFromList;
